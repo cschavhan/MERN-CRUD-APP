046dc22cd8332945929801332c69c36627304685
cschavhan <chandrakantchavhan01@gmail.com>
2024-03-24 00:13:48 +0530

    create signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,115 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 function SignUp() {
-  return <div>SignUp</div>;
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className=" flex flex-col justify-center items-center h-[90vh] bg-gray-500">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]"
+      >
+        <h1 className="text-center text-2xl font-bold">Registration Page</h1>
+
+        {/* fullname */}
+        <div className="flex flex-col gap-1">
+          <label htmlFor="username" className="font-semibold">
+            Username
+          </label>
+          <input
+            type="text"
+            required
+            name="username"
+            id="username"
+            placeholder="Enter your name..."
+            className="bg-transparent px-2 py-1 border"
+            value={user.username}
+            onChange={handleInput}
+          />
+        </div>
+
+        {/* email */}
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="font-semibold">
+            Email
+          </label>
+          <input
+            type="email"
+            required
+            name="email"
+            id="email"
+            placeholder="Enter your email..."
+            className="bg-transparent px-2 py-1 border"
+            value={user.email}
+            onChange={handleInput}
+          />
+        </div>
+        {/* phone */}
+        <div className="flex flex-col gap-1">
+          <label htmlFor="phone" className="font-semibold">
+            Phone
+          </label>
+          <input
+            type="number"
+            required
+            name="phone"
+            id="phone"
+            placeholder="Enter your phone number..."
+            className="bg-transparent px-2 py-1 border"
+            value={user.phone}
+            onChange={handleInput}
+          />
+        </div>
+        {/* password */}
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="font-semibold">
+            Password
+          </label>
+          <input
+            type="password"
+            required
+            name="password"
+            id="password"
+            placeholder="Enter your password..."
+            className="bg-transparent px-2 py-1 border"
+            value={user.password}
+            onChange={handleInput}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="mt-2 bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 rounded-sm py-2 font-semibold"
+        >
+          Create account
+        </button>
+        {/* if user already login then redirect user to login page */}
+        <p className="text-center">
+          Already have an account? {""}
+          <Link to="/login" className="link text-accent cursor-pointer">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
